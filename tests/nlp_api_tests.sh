baddfab408aefd1ec9e43b085efc14b98f6a4c38
sjin824 <sjin824@aucklanduni.ac.nz>
2025-02-02 05:10:15 +0000

    Finished testing and debuging stanza api.

--- tests/nlp_api_tests.sh
+++ tests/nlp_api_tests.sh
@@ -2,7 +2,7 @@
 
 # 定义 API 的 URL
 NLP_SPACY_API_URL="http://localhost:5004/nlp/spacy"
-# NLP_STANZA_API_URL="http://localhost:5004/nlp/stanza"
+NLP_STANZA_API_URL="http://localhost:5004/nlp/stanza"
 
 # 遍历 mock_data 文件夹中的所有 JSON 文件
 for json_file in tests/mock_data/*.json; do
@@ -12,7 +12,7 @@ for json_file in tests/mock_data/*.json; do
   echo "Testing NLP spaCy API..."
   curl -s -w "%{http_code}\n" -H "Content-Type: application/json" -d @"$json_file" "$NLP_SPACY_API_URL"
   
-  # echo "Testing NLP stanza API..."
-  # curl -s -w "%{http_code}\n" -H "Content-Type: application/json" -d @"$json_file" "$NLP_STANZA_API_URL"
+  echo "Testing NLP stanza API..."
+  curl -s -w "%{http_code}\n" -H "Content-Type: application/json" -d @"$json_file" "$NLP_STANZA_API_URL"
   echo -e "=====================================\n"
 done
